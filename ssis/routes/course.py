7f3c148c0e29829ea5ae3d757947c1737c4f811a
Carlo Arellano <carlo.arellano@g.msuiit.edu.ph>
2023-10-18 23:45:38 +0800

    add functionality done

--- ssis/routes/course.py
+++ ssis/routes/course.py
@@ -1,8 +1,21 @@
-from flask import Blueprint
-from flask import render_template
+from flask import Blueprint, request, render_template
+from ssis import mysql
 
 course_bp = Blueprint('course', __name__)
 
-@course_bp.route('/')
+@course_bp.route('/', methods=["GET", "POST"])
 def course_dashboard():
-    return render_template("course.html", courses=[])
+    if request.method == "POST":
+        course_code = request.form.get("course_code")
+        course_name = request.form.get("course_name")
+        college_code = request.form.get("college_code")
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO course (course_code, course_name, college_code) VALUES (%s, %s, %s)", 
+                    (course_code, course_name, college_code))
+        mysql.connection.commit()
+
+    cur = mysql.new_cursor(dictionary=True)
+    cur.execute("SELECT * FROM course")
+    courses = cur.fetchall()
+    
+    return render_template("course.html", courses=courses)
--- ssis/routes/student.py
+++ ssis/routes/student.py
@@ -1,8 +1,24 @@
-from flask import Blueprint
-from flask import render_template
+from flask import Blueprint, request, render_template
+from ssis import mysql
 
 student_bp = Blueprint('student', __name__)
 
-@student_bp.route('/')
+@student_bp.route('/', methods=["GET", "POST"])
 def student_dashboard():
-    return render_template("student.html", students=[])
+    if request.method == "POST":
+        student_id = request.form.get("student_id")
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
+        course_code = request.form.get("course_code")
+        year_level = request.form.get("year_level")
+        gender = request.form.get("gender")
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO student (student_id, first_name, last_name, course_code, year_level, gender) VALUES (%s, %s, %s, %s, %s, %s)",
+                    (student_id, first_name, last_name, course_code, year_level, gender))
+        mysql.connection.commit()
+
+    cur = mysql.new_cursor(dictionary=True)
+    cur.execute("SELECT * FROM student")
+    students = cur.fetchall()
+
+    return render_template("student.html", students=students)
