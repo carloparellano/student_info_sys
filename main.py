c15654b83e716a441d55bb3e19c550737d8ac3cd
Carlo Arellano <carlo.arellano@g.msuiit.edu.ph>
2023-10-26 17:22:25 +0800

    additional commit

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from ssis import create_app
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = create_app()
 
--- ssis/__init__.py
+++ ssis/__init__.py
@@ -1,11 +1,12 @@
+import os
 from flask import Flask, render_template
 from flask_mysql_connector import MySQL
 
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_DATABASE'] = 'student-info-sys'
-app.config['MYSQL_PASSWORD'] = '12345'
+app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
+app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
+app.config['MYSQL_DATABASE'] = os.getenv("MYSQL_DATABASE")
+app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
 mysql = MySQL(app)
 
 
