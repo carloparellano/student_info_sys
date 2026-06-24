c5af70bc60aa5687f96d2a4a023863900a1cada7
carloparellano <carlo.arellano@g.msuiit.edu.ph>
2026-06-24 13:58:02 +0800

    Enhance student ID input validation with placeholder, pattern, and error feedback in add/update forms

--- ssis/students/forms.py
+++ ssis/students/forms.py
@@ -4,6 +4,7 @@ from wtforms.validators import DataRequired, Regexp
 
 ID_PATTERN = r'^\d{4}-\d{4}$'
 
+
 class StudentForm(FlaskForm):
     student_id = StringField('Student ID', validators=[
         DataRequired(),
@@ -12,10 +13,10 @@ class StudentForm(FlaskForm):
     first_name = StringField('First Name', validators=[DataRequired()])
     last_name = StringField('Last Name', validators=[DataRequired()])
     course_code = SelectField('Course', validators=[DataRequired()])
-    year_level = StringField('Year Level', validators=[DataRequired()])
-    gender = SelectField(
-        'Gender',
-        choices=[('Male', 'Male'), ('Female', 'Female')],
-        validators=[DataRequired()]
-    )
+    year_level = SelectField('Year Level', choices=[
+        ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4')
+    ], validators=[DataRequired()])
+    gender = SelectField('Gender', choices=[
+        ('Male', 'Male'), ('Female', 'Female')
+    ], validators=[DataRequired()])
     student_url = StringField('Student Image URL')
